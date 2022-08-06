ba2ae0e191cec457b999b6567a60969c81f47176
Hamza Susam <hamza.susam@jetlink.io>
2022-08-06 17:20:44 +0300

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <Router>
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact strict path="/iletisim" element={<Iletisim/>}/>
+          </Routes>
+      </Router>
+    );
+  }
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Iletisim() {
+  return <h2>iletisim</h2>;
 }
 
 export default App;
